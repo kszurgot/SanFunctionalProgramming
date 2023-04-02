3998636391c2acfc1f9481980ba757a174b4a080
kszurgot <kamil.szurgot@int.pl>
2023-04-02 13:54:25 +0200

    Add 3.a

--- zad3.clj
+++ zad3.clj
@@ -1,15 +1,29 @@
-;;a TODO
+;; A
+(defn fib-steps [n]
+  (let
+    [counter (atom 0)]
+    ((defn fib [n]
+       (swap! counter inc)
+       (if (< n 2) n
+           (+' (fib (- n 1))
+              (fib (- n 2))
+           )
+       )
+     ) n)
+     @counter)
+)
+
+(fib-steps 5) ;; result: 15
 
 
 ;; B i C
 (defn fib+ [n]
   ((fn [a b i n]
-     (
-      if (= i n) b
-      (recur b (+' a b) (inc i) n)
+     (if (= i n) b
+         (recur b (+' a b) (inc i) n)
      )
-   ) 0 1 1 n)
+   )
+  0 1 1 n)
 )
-
 
 (fib+ 10000)
