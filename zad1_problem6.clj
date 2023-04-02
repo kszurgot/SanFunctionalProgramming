61ab9eeded4697517a441b25408b7a1f2c93b744
kszurgot <kamil.szurgot@int.pl>
2023-04-02 13:54:42 +0200

    Refactor task #1

--- zad1_problem6.clj
+++ zad1_problem6.clj
@@ -1,16 +1,15 @@
-(defn sum [fn n]
-  (if (= 1 n)
-    1 
-    (+ (fn n) (sum fn (dec n)))
-))
+(defn sum [funk n]
+  (if (= 1 n) 1
+      (+ (funk n) (sum funk (dec n)))
+  )
+)
 
 (defn square [n] (* n n))
 
-(defn do-nothing [n] n)
 
 (sum square 10)
-(square (sum do-nothing 10))
+(square (sum identity 10))
 
-(- (square (sum do-nothing 10)) (sum square 10))
+(- (square (sum identity 10)) (sum square 10))
 
-(- (square (sum do-nothing 100)) (sum square 100))
+(- (square (sum identity 100)) (sum square 100))
